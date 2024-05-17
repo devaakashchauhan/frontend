5fe46e20510e507598144c2b8cbc1519a7d26fbb
Aakash Chauhan <developeraakashchauhan@gmail.com>
2024-05-17 15:50:06 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-<<<<<<< HEAD
       "/api/v1": "https://backend-w48u.onrender.com",
-=======
-      "/api/v1": "https://backend-s3t3.onrender.com",
->>>>>>> 1d0bd9271d90bc787d817ac2bd7d8cecca650eda
     },
   },
   plugins: [react()],
