1d0bd9271d90bc787d817ac2bd7d8cecca650eda
Aakash Chauhan <developeraakashchauhan@gmail.com>
2024-05-17 13:54:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": "https://backend-6h1k.onrender.com",
+      "/api/v1": "https://backend-s3t3.onrender.com",
     },
   },
   plugins: [react()],
