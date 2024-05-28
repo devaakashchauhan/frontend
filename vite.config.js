57d1b9571545c180e9b2fb4a35023595f4c4d796
Aakash Chauhan <developeraakashchauhan@gmail.com>
2024-05-28 18:17:28 +0530

    change-proxy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": "https://backend-vlz2.onrender.com",
+      "/api/v1": {
+        target: "https://backend-gd4p.onrender.com",
+        changeOrigin: true, // Change the origin of the host header to the target URL
+        secure: false, // Allow proxying to a server with self-signed SSL certificates
+        rewrite: (path) => path.replace(/^\/api\/v1/, "/api/v1"), // Optional, can be used for custom path rewrites
+      },
     },
   },
   plugins: [react()],
